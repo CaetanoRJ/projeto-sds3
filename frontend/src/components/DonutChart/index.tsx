e37f57b9cadae57cfcd583e4d1ae7498c12c34c3
CaetanoRJ <caetanoc3@gmail.com>
2021-11-07 12:58:24 -0300

    Fix: Piechart with hooks

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Chart from "react-apexcharts";
 import { SaleSum } from "types/sale";
 import { BASE_URL } from "utils/requests";
@@ -10,17 +10,20 @@ type ChartData = {
 };
 
 const DonutChart = () => {
-  let chartData: ChartData = { labels: [], series: [] };
-
-  axios.get(`${BASE_URL}/sales/amount-by-seller`).then((response) => {
-    const data = response.data as SaleSum[];
-    const myLabels = data.map((x) => x.sellerName);
-    const mySeries = data.map((x) => x.sum);
-
-    chartData = { labels: myLabels, series: mySeries };
-    console.log(response.data);
-    console.log(chartData);
+  const [chartData, setChartData] = useState<ChartData>({
+    labels: [],
+    series: [],
   });
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales/amount-by-seller`).then((response) => {
+      const data = response.data as SaleSum[];
+      const myLabels = data.map((x) => x.sellerName);
+      const mySeries = data.map((x) => x.sum);
+
+      setChartData({ labels: myLabels, series: mySeries });
+    });
+  }, []);
 
   /*  const mockData = {
     series: [477138, 499928, 444867, 220426, 473088],
