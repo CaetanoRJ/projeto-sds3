426ad8fea75e0b54b036692f6403f2d873fad668
CaetanoRJ <caetanoc3@gmail.com>
2021-11-07 15:30:17 -0300

    feat: Dynamic pagination

--- frontend/src/page/Dashboard/index.tsx
+++ frontend/src/page/Dashboard/index.tsx
@@ -10,21 +10,20 @@ const Dashboard = () => {
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary">Dahborad de Venda</h1>
+        <h1 className="text-primary">Dashboard de Venda</h1>
         <div className="row px-3">
           <div className="col-sm-6">
             <h5 className="text-center text-secondary">
               Taxa de sucesso vendass
+              <BarChart />
             </h5>
-            <BarChart />
           </div>
-          <div className="col-sm-6">
+          <div className="col-sm-6 text-center">
             <h5>Todas vendass</h5>
             <DonutChart />
           </div>
+          <DataTable />
         </div>
-
-        <DataTable />
       </div>
       <Footer />
     </>
